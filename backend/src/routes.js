fae06fccb6803470bc1046b01a4f4c7ea05219a6
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-08-23 08:46:18 -0300

    added JWT and Yup

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,16 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'João Phellipe',
-    email: 'sr.phellipe@gmail.com',
-    password_hash: '123456',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
